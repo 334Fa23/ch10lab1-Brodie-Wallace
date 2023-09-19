e36a29a4191669abd7b11b510051eed9e93d002c
Brodie-Wallace <brodiemwallace@gmail.com>
2023-09-19 17:29:02 -0500

    Ch10Lab

---
+++ script.js
@@ -0,0 +1,13 @@
+/*Student Name: Brodie Wallace
+File Name: script.js
+Date: 9-19-2023
+*/
+//Hamburger menu function
+function hamburger () {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+            menu.style.display = "none";
+    } else {
+        menu.style.display ="block";
+    }
+}
